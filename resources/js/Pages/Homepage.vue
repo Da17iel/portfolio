4e558535758e91b441e90fbe502dbe99989565e2
Daniel Greil <danimickigreil@gmail.com>
2024-08-05 10:59:52 +0200

    Added basic homepage file

---
+++ resources/js/Pages/Homepage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BasicLayout from "@/Layouts/BasicLayout.vue";
+
+defineOptions({
+    layout: BasicLayout,
+})
+
+defineProps({
+    laravelVersion: String,
+    phpVersion: String,
+})
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
