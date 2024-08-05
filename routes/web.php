f821a337c0310db447282c74111e2c30eb18a303
Daniel Greil <danimickigreil@gmail.com>
2024-08-05 11:29:02 +0200

    Added default pages

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,18 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/kontakt', function () {
+    return Inertia::render('Contact');
+});
+
+Route::get('/ueber-mich', function () {
+    return Inertia::render('AboutMe');
+});
+
+Route::get('/erfahrung', function () {
+    return Inertia::render('Experience');
+});
+
+Route::get('/referenzen', function () {
+    return Inertia::render('References');
+});
