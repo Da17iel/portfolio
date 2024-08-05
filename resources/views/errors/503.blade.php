6b783f34e10b9a445c0e643bd3c1f0e82c5e1859
Daniel Greil <danimickigreil@gmail.com>
2024-08-05 10:52:03 +0200

    Added maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+    <title>Be right back.</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato', sans-serif;
+        }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+        .title {
+            font-size: 72px;
+            font-weight: bold;
+            margin-bottom: 40px;
+        }
+        .subTitle {
+            font-size: 25px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">Wartungsmodus aktiv<br>Updates sind im Gange</div>
+        <p class="subTitle">Bitte warten Sie einen Augenblick...</p>
+    </div>
+</div>
+</body>
+</html>
