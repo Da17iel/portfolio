55d7d9555d7b1278f75f67cc36a5a327121088bd
Da17iel <68570543+Da17iel@users.noreply.github.com>
2024-09-25 22:19:27 +0200

    Added components for Email and GitHub

--- resources/js/Components/ContactMeEMail.vue
+++ resources/js/Components/ContactMeEMail.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-
+    <div class="my-24 flex flex-col lg:flex-row gap-4 lg:gap-20">
+        <div class="animate-hover min-w-52 max-lg:max-w-52">
+            <a href="https://www.linkedin.com/" target="_blank">
+                <img src="/images/EmailIcon.png" alt="Email Icon">
+            </a>
+        </div>
+        <div class="w-full">
+            <p class="text-3xl">E-Mail</p>
+            <p class="my-3">
+                Per E-Mail bin ich natürlich auch sehr gut erreichbar.
+                <br>Falls Sie in ein Telefongespräch mit mir interessiert sind, können Sie hier nach meiner Tel. Nummer
+                Fragen.
+                <br>Ich freue mich bereits sehr auf Ihr E-Mail
+            </p>
+            <a href="mailto:danielgreil2004@gmail.com">Daniel Greil E-Mail Adresse: danielgreil2004@gmail.com</a>
+        </div>
+    </div>
 </template>
 
 <style scoped>
--- resources/js/Components/ContactMeGitHub.vue
+++ resources/js/Components/ContactMeGitHub.vue
@@ -3,7 +3,24 @@
 </script>
 
 <template>
-
+    <div class="my-24 flex flex-col lg:flex-row gap-4 lg:gap-20">
+        <div class="w-full">
+            <p class="text-3xl">GitHub</p>
+            <p class="my-3">
+                Die GitHub Plattform brauch zu diesem Punkt keine Vorstellung. Jeder Developer, der in den letzten
+                Jahren aktiv am programmieren war, hat bereits mehrmals GitHub gebraucht.
+                <br>Und so auch ich. GitHub ist die Plattform, bei der ich meinen Code hochlade. Ich arbeite regelmässig
+                auf GitHub. Geschäftlich und Privat!
+                <br>Sie können mich gerne auch auf GitHub kontaktieren.
+            </p>
+            <a href="https://github.com/Da17iel" target="_blank">GitHub Daniel Greil Dev. Profil</a>
+        </div>
+        <div class="animate-hover min-w-52 max-lg:max-w-52">
+            <a href="https://www.linkedin.com/" target="_blank">
+                <img src="/images/GitHubIcon.png" alt="GitHub Icon">
+            </a>
+        </div>
+    </div>
 </template>
 
 <style scoped>
